25c3f03c3a3ec5e5f074b90a72625573a5d0aeba
CalebBird2000 <167284387+CalebBird2000@users.noreply.github.com>
2025-11-02 14:20:04 -0500

    Created Star.cs

---
+++ ZTF Explorer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZTF_Explorer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ZTF Explorer/Star.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTF_Explorer
+{
+    public class Star
+    {
+        public string ObjectID;
+        public float ra;
+        public float decl;
+        public Boolean Checked;
+        public Boolean AAVSO;
+    }
+}
