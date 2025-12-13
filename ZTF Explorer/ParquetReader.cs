99b1bba48479e3f33d06eab1a0fe4a770af7ed68
CalebBird2000 <167284387+CalebBird2000@users.noreply.github.com>
2025-12-13 11:51:12 -0500

    Added some comments

--- ZTF Explorer/ParquetReader.cs
+++ ZTF Explorer/ParquetReader.cs
@@ -21,16 +21,20 @@ namespace ZTF_Explorer
                 using var rowGroupReader = file.RowGroup(rowGroup);
                 var groupNumRows = checked((int)rowGroupReader.MetaData.NumRows);
 
+
+                //Read variables from parquet file
                 var Column0 = rowGroupReader.Column(0).LogicalReader<long?>(); //read ObJID
                 var Column2 = rowGroupReader.Column(2).LogicalReader<short?>(); //read FieldID
                 var Column4 = rowGroupReader.Column(4).LogicalReader<float?>(); //read RA
                 var Column5 = rowGroupReader.Column(5).LogicalReader<float?>();// read DEC
 
+                //Assign variables
                 var Objid = Column0.ReadAll(groupNumRows);
                 var Fieldid = Column2.ReadAll(groupNumRows);
                 var ObjRA = Column4.ReadAll(groupNumRows);
                 var ObjDec = Column5.ReadAll(groupNumRows);
 
+                //Loop that reads each row and creates a star object
                 for (int i = 0; i != 100 /*groupNumRows*/; ++i)
                 {
                     Console.WriteLine(i);
@@ -44,7 +48,7 @@ namespace ZTF_Explorer
                     Console.WriteLine(ObjDec[i]);
                     */
                     Star star = new(Convert.ToDouble(Objid[i]), Convert.ToInt32(Fieldid[i]), objRA, objDec, false, false);
-
+                    Console.WriteLine("OBJ RA: " + objRA);
                     Queue.StarsQ.Enqueue(star);
                     LightCurveRead(Convert.ToDouble(Objid[i]), i,rowGroupReader);
                     Console.WriteLine("Stars queue " + Queue.StarsQ.Count);
@@ -59,17 +63,18 @@ namespace ZTF_Explorer
         {
             var groupNumRows = checked((int)rowGroupReader.MetaData.NumRows);
 
-            var Column1 = rowGroupReader.Column(1).LogicalReader<sbyte?>();
-            var Column7 = rowGroupReader.Column(7).LogicalReader<double?[]>();
-            var Column8 = rowGroupReader.Column(8).LogicalReader<float?[]>();
-            var Column9 = rowGroupReader.Column(9).LogicalReader<float?[]>();
+            var Column1 = rowGroupReader.Column(1).LogicalReader<sbyte?>(); //FilterID
+            var Column7 = rowGroupReader.Column(7).LogicalReader<double?[]>(); //HMJD
+            var Column8 = rowGroupReader.Column(8).LogicalReader<float?[]>(); //Mag
+            var Column9 = rowGroupReader.Column(9).LogicalReader<float?[]>();//Magerr
 
-
+            //Assign variables
             var Filterid = Column1.ReadAll(groupNumRows);
             var Hmjd = Column7.ReadAll(groupNumRows);
             var Mag = Column8.ReadAll(groupNumRows);
             var Magerr = Column9.ReadAll(groupNumRows);
 
+            //Loop that reads each row and creates a light curve object
             for (int i = 0; i < Mag[Row].Length; i++)
             {
                 LightCurve lightCurve = new(Objid, Convert.ToInt32(Filterid[Row]), Convert.ToDouble(Hmjd[Row][i]), Convert.ToDouble(Mag[Row][i]), Convert.ToDouble(Magerr[Row][i]));
