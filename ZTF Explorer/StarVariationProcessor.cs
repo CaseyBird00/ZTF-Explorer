53758ddf7a41ce9b710bd7b23bfb1fd08df57f90
CalebBird2000 <167284387+CalebBird2000@users.noreply.github.com>
2025-12-14 17:37:21 -0500

    Undo

--- ZTF Explorer/StarVariationProcessor.cs
+++ ZTF Explorer/StarVariationProcessor.cs
@@ -12,44 +12,69 @@ namespace ZTF_Explorer
     {
         public static void MatchStars(Star star)
         {
-            double tolerance = 0.00833; // 1 arcsecond in degrees
+            int batchSize = 100;
+            var stars = Queue.VariableStarsQ;
+            int totalStars = stars.Count;
+            int processed = 0;
 
-            double raMin = star.Ra - tolerance;
-            double raMax = star.Ra + tolerance;
-
-            double decMin = star.Decl - tolerance;
-            double decMax = star.Decl + tolerance;
-
-
-            var conn = SQL.GetConnection();
-            using var cmd = conn.CreateCommand();
-            cmd.CommandText = @"
-    SELECT *
-    FROM Stars
-    WHERE RA BETWEEN @raMin AND @raMax
-      AND DECLI BETWEEN @decMin AND @decMax";
-
-            cmd.Parameters.AddWithValue("@raMin", raMin);
-            cmd.Parameters.AddWithValue("@raMax", raMax);
-            cmd.Parameters.AddWithValue("@decMin", decMin);
-            cmd.Parameters.AddWithValue("@decMax", decMax);
-
-            try
+            using var conn = SQL.GetConnection();
+            while (processed < /*totalStars*/ 50)
             {
-                Console.WriteLine("Comparing RA " + star.Ra +"DECLI" + star.Decl);
-                using var reader = cmd.ExecuteReader();
 
-                while (reader.Read())
+                Console.WriteLine("adding to batch");
+                var batch = stars.Skip(processed).Take(batchSize).ToList();
+                var sqlBuilder = new StringBuilder("SELECT * FROM Stars WHERE ");
+
+                var parameters = new List<MySqlConnector.MySqlParameter>();
+
+                for (int i = 0; i < /*batch.Count*/ 20; i++)
                 {
-                    Console.WriteLine($"Found Star: {reader["vsx_id"]}, RA: {reader["RA"]}, Dec: {reader["DECLI"]}");
+                    var CurrentStar = batch[i];
+                    double tolerance = 0.00833; // 30 arcseconds for testing
+
+                    Console.WriteLine(CurrentStar + "number " + i);
+
+                    double raMin = CurrentStar.Ra - tolerance;
+                    double raMax = CurrentStar.Ra + tolerance;
+                    double decMin = CurrentStar.Decl - tolerance;
+                    double decMax = CurrentStar.Decl + tolerance;
+
+                    sqlBuilder.Append($"(RA BETWEEN @raMin{i} AND @raMax{i} AND DECLI BETWEEN @decMin{i} AND @decMax{i})");
+
+                    if (i < batch.Count - 1)
+                        sqlBuilder.Append(" OR ");
+
+                    parameters.Add(new MySqlConnector.MySqlParameter($"@raMin{i}", raMin));
+                    parameters.Add(new MySqlConnector.MySqlParameter($"@raMax{i}", raMax));
+                    parameters.Add(new MySqlConnector.MySqlParameter($"@decMin{i}", decMin));
+                    parameters.Add(new MySqlConnector.MySqlParameter($"@decMax{i}", decMax));
                 }
 
-            } catch(Exception ex) { 
-             Console.WriteLine(ex.Message);
-            
+                Console.WriteLine("Querying Stars");
+
+                using var cmd = conn.CreateCommand();
+                cmd.CommandTimeout = 1800;
+                cmd.CommandText = sqlBuilder.ToString();
+                cmd.Parameters.AddRange(parameters.ToArray());
+
+                try
+                {
+                    using var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Found Star: {reader["vsx_id"]}, RA: {reader["RA"]}, Dec: {reader["DECLI"]}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                processed += batchSize;
+                Console.WriteLine($"Processed {processed}/{totalStars} stars...");
             }
-            
-            
+
+
         }
 
     }
