ed1807694ffdff078a70427d03e0df62a7126449
CalebBird2000 <167284387+CalebBird2000@users.noreply.github.com>
2025-12-13 21:22:05 -0500

    Begun work on SQL.cs. Added .dat to gitignore

--- ZTF Explorer/SQL.cs
+++ ZTF Explorer/SQL.cs
@@ -3,21 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Azure.Core.GeoJson;
 using Microsoft.Data.SqlClient;
 
 namespace ZTF_Explorer
 {
     public class SQL
     {
-        public static void Connection()
+        string IP { get; set; }
+        string UserID { get; set; }
+        string Password { get; set; }
+
+        string Database { get; set; }
+        void LoadSQL()
         {
-            var server = new SqlConnectionStringBuilder
+            string FilePath = "C:\\Users\\Casey\\Documents\\GitHub\\ZTF-Explorer\\ZTF Explorer\\SQL.dat";
+
+            var file = File.ReadAllLines(FilePath);
+
+            IP = file[0];
+            UserID = file[1];
+            Password = file[2];
+            Database = file[3];
+
+        }
+        public void Connection()
+        {
+            var builder = new SqlConnectionStringBuilder
             {
-                DataSource = "ztf-explorer-server.database.windows.net",
-                UserID = "ztfexploreradmin",
-                Password = "ZtfExplorer123!",
-                InitialCatalog = "ZTF-Explorer-DB"
+                DataSource = IP,           // IP or server name
+                UserID = UserID,           // SQL username
+                Password = Password,       // SQL password
+                InitialCatalog = Database  // Database name
             };
         }
+
     }
 }
